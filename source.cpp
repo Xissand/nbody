4c68b5177eac5b3c864032a0f7de18584251c805
Xissand <xissand@gmail.com>
2020-02-15 14:44:49 +0300

    Periodic boundaries implemented

--- source.cpp
+++ source.cpp
@@ -14,8 +14,10 @@ float q[N][3];
 float v[N][3];
 float a[N][3];
 float T = 0, P = 0, E = 0;
-int step = 0, total = 10000;
+int step = 0, total = 2000;
 int t = 0, dt = 1, t_diag = 5, t_snap = 5;
+
+float cell_size = 500;
 
 ofstream dump;
 ofstream diag;
@@ -25,6 +27,8 @@ void generate();
 void interract(int i, int j);
 
 void evolve();
+
+void boundaries(int i);
 
 void diagnose();
 
@@ -46,7 +50,7 @@ int main()
     snapshot();
     // diagnose();
 
-    //total = -1;
+    // total = -1;
     while (step < total)
     {
         evolve();
@@ -66,9 +70,9 @@ void generate()
             for (int k = 0; k < 8; k++)
             {
                 int n = i * 8 * 8 + j * 8 + k;
-                q[n][0] = 10*i;
-                q[n][1] = 10*j;
-                q[n][2] = 10*k;
+                q[n][0] = 10 * i;
+                q[n][1] = 10 * j;
+                q[n][2] = 10 * k;
                 v[n][0] = 0.0;
                 v[n][1] = 0.0;
                 v[n][2] = 0.0;
@@ -79,15 +83,15 @@ void generate()
 
 void interract(int i, int j)
 {
-    if(i ==j)
-      return;
+    if (i == j)
+        return;
     float r[3] = {0.0, 0.0, 0.0};
     r[0] = q[i][0] - q[j][0];
     r[1] = q[i][1] - q[j][1];
     r[2] = q[i][2] - q[j][2];
 
-    float k = pow((r[0] * r[0] + r[1] * r[1] + r[2] * r[2]), 1.5);
-    float p = pow((r[0] * r[0] + r[1] * r[1] + r[2] * r[2]), -0.5);
+    float k = pow((r[0] * r[0] + r[1] * r[1] + r[2] * r[2] + 0.1), 1.5);
+    float p = pow((r[0] * r[0] + r[1] * r[1] + r[2] * r[2] +0.1), -0.5);
     P += p;
 
     for (int iter = 0; iter < 3; iter++)
@@ -103,9 +107,9 @@ void evolve()
 {
     for (int i = 0; i < N; i++)
     {
-      a[i][0] = 0.0;
-      a[i][1] = 0.0;
-      a[i][2] = 0.0;
+        a[i][0] = 0.0;
+        a[i][1] = 0.0;
+        a[i][2] = 0.0;
     }
     for (int i = 0; i < N; i++)
     {
@@ -120,10 +124,12 @@ void evolve()
         for (int j = 0; j < 3; j++)
         {
             v[i][j] += a[i][j] * dt;
-            q[i][j] += v[i][j] * dt + a[i][j] * dt *dt / 2;
+            q[i][j] += v[i][j] * dt + a[i][j] * dt * dt / 2;
         }
+        boundaries(i);
     }
-    t+=dt;
+
+    t += dt;
 
     if ((t % t_diag) == 0)
         diagnose();
@@ -135,6 +141,22 @@ void evolve()
     step++;
 }
 
+void boundaries(int i)
+{
+    if (q[i][0] < -cell_size * 0.5)
+        q[i][0] += cell_size;
+    if (q[i][0] > cell_size * 0.5)
+        q[i][0] -= cell_size;
+    if (q[i][1] < -cell_size * 0.5)
+        q[i][1] += cell_size;
+    if (q[i][1] > cell_size * 0.5)
+        q[i][1] -= cell_size;
+    if (q[i][2] < -cell_size * 0.5)
+        q[i][2] += cell_size;
+    if (q[i][2] > cell_size * 0.5)
+        q[i][2] -= cell_size;
+}
+
 void diagnose()
 {
     for (int i = 0; i < N; i++)
@@ -142,7 +164,7 @@ void diagnose()
         float vel2 = (v[i][0] * v[i][0] + v[i][1] * v[i][1] + v[i][2] * v[i][2]);
         T += vel2 / 2;
     }
-    E=T+P;
+    E = T + P;
     cout << "Step: " << step << " P=" << P << " T=" << T << " E=" << E << endl;
 }
 
