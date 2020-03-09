7634018b75e5dbea7832031c01a8cf2d8bd7e9f9
Xissand <nikitiuk.bi@phystech.edu>
2020-03-09 08:43:21 +0300

    Report on pressure updated

--- src/units.h
+++ src/units.h
@@ -1,10 +1,34 @@
 #pragma once
 
+//ro = 0.6
+//constexpr int N = 200;
+//constexpr float cell_size = 3.466806372;
+
+//constexpr int N = 250;
+//constexpr float cell_size = 3.734503955;
+
+//constexpr int N = 300;
+//constexpr float cell_size = 3.96850263;
+
+//constexpr int N = 350;
+//constexpr float cell_size = 4.177748279;
+
+//constexpr int N = 400;
+//constexpr float cell_size = 4.367902324;
+
+//constexpr int N = 500;
+//constexpr float cell_size = 4.705180144;
+
+constexpr int N = 600;
+constexpr float cell_size = 5.0f;
+
+//constexpr int N = 700;
+//constexpr float cell_size = 5.263632998;
+
 //ro = N/(2cell_size)**3
 
-constexpr int N = 512;
-
-constexpr float cell_size = 4.142976675;// ro = 0.9
+//constexpr int N = 512;
+//constexpr float cell_size = 4.142976675;// ro = 0.9
 //constexpr float cell_size = 4.30886938;// ro = 0.8
 //constexpr float cell_size = 4.504991522;// ro = 0.7
 //constexpr float cell_size = 4.742524406; //to= 0.6
@@ -27,7 +51,7 @@ constexpr int snap_steps = 50;
 constexpr int thermo_steps = 10;
 constexpr float dt = 1e-6; 
 
-constexpr int BLOCK_SIZE = 128;
+constexpr int BLOCK_SIZE = 50;
 
 constexpr float NA = 1;
 constexpr float K_B = 1;
